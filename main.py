ccf6ae364961c37cefe1ee63a33f4b70a7879260
Alok Swain <alokswain1@gmail.com>
2023-03-28 13:10:29 -0500

    Main file to execute the other files

---
+++ main.py
@@ -0,0 +1,16 @@
+from mcmc_demo0 import *
+from mcmc_demo1 import *
+from mcmc_demo2 import *
+from mcmc_demo3 import *
+
+"""
+import mcmc_demo0
+import mcmc_demo1
+import mcmc_demo2
+import mcmc_demo3
+"""
+if __name__ == '__main__':
+    pass 
+    #one.foo()
+    #two.bar()
+    #three.baz()
